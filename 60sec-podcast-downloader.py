88928f2c05922710fc9bfa50914a4f96b34c925f
JackWang <jy821022@gmail.com>
2017-03-17 00:53:59 +0800

    fix some bugs

--- 60sec-podcast-downloader.py
+++ 60sec-podcast-downloader.py
@@ -2,7 +2,7 @@ try:
 	import threading
 except ImportError:
 	import dummy_threading as threading
-import requests, urllib, sys, os, re, queue, time
+import requests, urllib, sys, os, re, queue, time, math
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup as Soup
 
@@ -37,7 +37,7 @@ class PodcastParser(HTMLParser):
 def add_history(id, file_name):
 	if id not in historyId:
 		print("store %s to history" % id)
-		historyIdtoAdd.append(id)
+		historyIdtoAdd.append(id+'\n')
 		idPool.put(dict(id=id, name=file_name))
 
 # Inspect acceptable characters on Windows
@@ -63,7 +63,7 @@ def download_podcast():
 			for chunk in r.iter_content(chunk_size=1024):
 				if chunk:
 					f.write(chunk)
-		num_files++
+		num_files+=1
 		idPool.task_done()
 
 def fetch_url(url, page_no):
@@ -71,13 +71,16 @@ def fetch_url(url, page_no):
 	return requests.get(url, params=payload)
 
 def main():
+	time_start = time.time()
 	url = "https://www.scientificamerican.com/podcasts/"
 	r = fetch_url(url, 2)
 	#http://marsray.pixnet.net/blog/post/61040521-%5Bpython3%5D-%E7%94%A8-python3-%E5%AF%AB%E4%B8%80%E5%80%8B%E7%B6%B2%E8%B7%AF%E7%88%AC%E8%9F%B2
 	if not os.path.exists('history.txt'):
 		print('create history.txt')
 		open('history.txt', 'w').close()
-	file_history = open('history.txt', 'r+')
+
+	history_file = open('history.txt', 'r+')
+	historyId = history_file.read().splitlines()
 
 	soup = Soup(r.text.encode(sys.stdin.encoding, 'replace').decode(sys.stdin.encoding), 'html.parser')
 	divArr = soup.find_all('div', attrs={"data-podcast-type": "gridded-podcast"})
@@ -88,18 +91,26 @@ def main():
 		podcast_id = download_tag['href'].split('fileId=')[1]
 		add_history(podcast_id, title)
 
+	print("====================================================")	
+	print("Start downloading")
+
 	# set threads
 	for i in range(num_threads):
 		thrd = threading.Thread(target=download_podcast)
 		thrd.setDaemon(True)
 		thrd.start()
 
-	idPool.join()
+	# write history
+	history_file.writelines(historyIdtoAdd)
+	history_file.close()
 
+	idPool.join()
+	time_end = time.time()
+	print("====================================================")	
 	# (1)number of files
 	print('%s files downloaded.' % num_files)
 	# (2)total download time
-	# print('Total downloading duration: %s' % )
+	print('Total downloading duration: %s' % round(time_end-time_start))
 
 if __name__ == "__main__":
 	main()
